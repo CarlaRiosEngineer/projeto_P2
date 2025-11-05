55ea2a6e419418b8e2a50b6a5acca6daeb515999
Carla Rios <carlariosndh@gmail.com>
2025-11-04 21:34:49 -0300

    adição do método exibirDetalhes
    Added a method to display exercise details.

--- Exercicio.java
+++ Exercicio.java
@@ -28,16 +28,23 @@ public class Exercicio {
     public void setSeries(int series) { this.series = series; }
     public void setTempoSegundos(int tempoSegundos) { this.tempoSegundos = tempoSegundos; }
 
-   
+    
     public double calcularVolume() {
         return peso * repeticoes * series;
     }
 
-   
+    public void exibirDetalhes(){
+        System.out.println("Exercicio: " + nome);
+        System.out.println("Séries x Repeticões: " + series  " x " + repeticoes);
+        System.out.println("Peso por repetição: " + peso + " Kg");
+        System.out.println("Tempo por série: " + tempoSegundos + " s");
+        System.out.println("Volume (carga) total: " + calcularVolume() + " kg");
+        
     public String toString() {
         return nome + " - " + series + "x" + repeticoes + " - " + peso + "kg, tempo por série: " + tempoSegundos + "s";
     }
 }
+
 
 
 
