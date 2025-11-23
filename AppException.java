2c42a12161e66a12a235607ba1333867523fdd7b
Carla Rios <carlariosndh@gmail.com>
2025-11-23 16:58:05 -0300

    apagamento de um código
    Removed the second constructor and unnecessary whitespace.

--- AppException.java
+++ AppException.java
@@ -3,11 +3,8 @@ public class  AppException extends Exception {
     public AppException(String mensagem) {
         super(mensagem);
     }
-
-    public AppException(String mensagem, Throwable causa) {
-        super(mensagem, causa);
-    }
 }
  
     
+
 
